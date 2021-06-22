26f6d7277d2c9fd3b95773a626392075b7a58d4b
Sujin Sam <sujinsam.dodge@gmail.com>
2021-06-22 13:31:38 +0530

    exit users if all values are allocated

--- src/test/scala/basicSimulation.scala
+++ src/test/scala/basicSimulation.scala
@@ -7,16 +7,19 @@ class basicSimulation extends Simulation{
         .baseUrl("https://reqres.in")
 
     val userRecords = csv("userList.csv").readRecords
-    val allocateCount = 5 // this number denotes the number of data allocated to each user
+    val allocateCount = 6 // this number denotes the number of data allocated to each user
 
     val scn = scenario("basic")
         .exec(session => session.set("feedCount", 0))
-        .repeat(5){
-            exec(session=>{
-                session
-                .set("p_user", userRecords(((session.userId).toInt * allocateCount) - allocateCount + session("feedCount").as[Int]).get("p_user").get)
-                .set("feedCount", (session("feedCount").as[Int] + 1))
+        .repeat(7){
+            exec(session => {
+                if(session("feedCount").as[Int] == allocateCount) session.markAsFailed    
+                else
+                    session
+                    .set("p_user", userRecords(((session.userId).toInt * allocateCount) - allocateCount + session("feedCount").as[Int]).get("p_user").get)
+                    .set("feedCount", (session("feedCount").as[Int] + 1))
             })
+            .exitHereIfFailed
             .exec(http("createUsers")
                 .post("/api/users")
                 .body(StringBody("""{"name": "${p_user}","job": "leader"}""")))
