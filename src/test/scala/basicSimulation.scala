01bf241f6751bd53f5be8e2f27b84aeb4627efba
Sujin Sam <sujinsam.dodge@gmail.com>
2021-06-22 15:13:45 +0530

    added feature for cycling data at EOF

--- src/test/scala/basicSimulation.scala
+++ src/test/scala/basicSimulation.scala
@@ -8,12 +8,20 @@ class basicSimulation extends Simulation{
 
     val userRecords = csv("userList.csv").readRecords
     val allocateCount = 6 // this number denotes the number of data allocated to each user
+    val isCircular = true // true, if the data should be cycled at EOF
 
     val scn = scenario("basic")
         .exec(session => session.set("feedCount", 0))
-        .repeat(7){
+        .repeat(8){
             exec(session => {
-                if(session("feedCount").as[Int] == allocateCount) session.markAsFailed    
+                if(session("feedCount").as[Int] == allocateCount){
+                    if(isCircular == true){
+                        session
+                        .set("p_user", userRecords(((session.userId).toInt * allocateCount) - allocateCount).get("p_user").get)
+                        .set("feedCount", 1)
+                    }
+                    else session.markAsFailed
+                }    
                 else
                     session
                     .set("p_user", userRecords(((session.userId).toInt * allocateCount) - allocateCount + session("feedCount").as[Int]).get("p_user").get)
